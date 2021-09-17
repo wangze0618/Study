03cac9707278205a0a6bd3e5e5349f3ca8883c64
wangze0618 <2867273828@qq.com>
2021-09-17 11:28:51 +0800

    lunbo 90%

--- pyg/js/index.js
+++ pyg/js/index.js
@@ -3,19 +3,20 @@ window.addEventListener('load', function () {
     const arrowL = this.document.querySelector('#lists1');
     const arrowR = this.document.querySelector('#lists2');
     const imgLength = lunbo.children.length;
-    const footList = this.document.querySelector('.footList')
+    const footList = this.document.querySelector('.footList');
+    const imgWidth = lunbo.children[0].offsetWidth;
 
-    lunbo.addEventListener('mouseenter', function () {
-        arrowL.style.display = 'block'
-        arrowR.style.display = 'block'
-    })
-    lunbo.addEventListener('mouseleave', function () {
-        arrowL.style.display = 'none'
-        arrowR.style.display = 'none'
-    })
+    // lunbo.addEventListener('mouseenter', function () {
+    //     arrowL.style.display = 'block'
+    //     arrowR.style.display = 'block'
+    // })
+    // lunbo.addEventListener('mouseleave', function () {
+    //     arrowL.style.display = 'none'
+    //     arrowR.style.display = 'none'
+    // })
 
 
-    for (let index = 0; index < imgLength; index++) {
+    for (let index = 0; index < imgLength - 1; index++) {
         const foots = this.document.createElement('li');
         footList.appendChild(foots);
         foots.setAttribute('index', index);
@@ -28,10 +29,32 @@ window.addEventListener('load', function () {
             }
             this.className = 'current'
 
-            let step = lunbo.children[0].offsetWidth * indexs;
+            let step = imgWidth * indexs;
             animate(lunbo, -step)
         })
     }
-    footList.children[0].className = 'current'
+    footList.children[0].className = 'current';
+
+    let flag = 0;
+    arrowR.addEventListener('click', function () {
+        if (flag == imgLength - 1) {
+            lunbo.style.left = 0;
+            flag = 0
+        }
+        flag++;
+        animate(lunbo, -flag * imgWidth);
+    })
+
+
+
+    arrowL.addEventListener('click', function () {
+        if (flag == imgLength - 1) {
+            lunbo.style.left = 0;
+            flag = 0
+        }
+        flag++;
+        animate(lunbo, flag * imgWidth);
+    })
+
 
 })
