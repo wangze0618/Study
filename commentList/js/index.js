bf55c42ac98bcd10d9ab13f97332fb883ed91445
wangze0618 <2867273828@qq.com>
2021-10-23 22:06:55 +0800

    taobao debounce

--- commentList/js/index.js
+++ commentList/js/index.js
@@ -4,6 +4,7 @@ $(function () {
 
 
 
+
     // 获取评论
     // 渲染评论 到页面上
     let array = [];
@@ -20,27 +21,33 @@ $(function () {
                         `<li class="list-group-item">
                         <span class="badge badge-yellow">${con.time}</span>
                         <span class="badge badge-blue">${con.username}</span>
-                         ${con.content}
-                    </li>`)
-                    $('.list-group').empty().append(array.join(''))
+                        ${con.content}
+                        </li>`)
                 })
+                $('.list-group').empty().append(array)
             }
         });
     }
-
     getComment()
+    // 发表评论
+    $('#form1').on('submit', function (e) {
+        e.preventDefault()
+        let data = $(this).serialize();
 
 
+        $.ajax({
+            type: "post",
+            url: "http://www.liulongbin.top:3006/api/addcmt",
+            data: data,
+            dataType: "json",
+            success: function (res) {
+                if (res.status !== 201) {
+                    alert(res.status)
+                }
+                getComment()
+            }
+        });
 
-
-
-
-
-
-
-
-
-
-
+    })
 
 })
